ff09630bccf0ccaca67d54143fee24dbefecc2eb
ekenulokamsi <kamsiyochi.ekenulo@pau.edu.ng>
2025-10-06 15:39:41 +0100

    week 2 practice 1

---
+++ week-2/practice_1.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Welcome to CSC 101!");
+    println!("The course leaarning outcome is as follows:");
+    
+    println!("1. Distinguish between computer and computer programming,/n
+2. Develop some techniques in computer science,/n
+3. Understand the different areas of study in computer science,/n
+4. Convertsant with applications of computer scieence,/n
+5. Navigate the career prospeects in computer sscience,/n
+6. Conversant with computer programming concepts.");
+}
