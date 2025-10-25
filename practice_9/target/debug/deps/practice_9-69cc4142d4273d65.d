b217e64ec8252df59ee1733cb9f867e7171b0967
ekenulokamsi <ekenulokamsi@gmail.com>
2025-10-25 14:37:50 +0100

    Delete practice_9 directory

--- practice_9/target/debug/deps/practice_9-69cc4142d4273d65.d
+++
@@ -1,5 +0,0 @@
-/home/ekenulokamsi/k.ekenuloCOS101/practice_9/target/debug/deps/practice_9-69cc4142d4273d65.d: src/main.rs
-
-/home/ekenulokamsi/k.ekenuloCOS101/practice_9/target/debug/deps/practice_9-69cc4142d4273d65: src/main.rs
-
-src/main.rs:
--- practice_9/target/debug/practice_9.d
+++
@@ -1,1 +0,0 @@
-/home/ekenulokamsi/k.ekenuloCOS101/practice_9/target/debug/practice_9: /home/ekenulokamsi/k.ekenuloCOS101/practice_9/src/main.rs
