4b3a2686a233494dcbc29aea77165dcd71398b4c
ekenulokamsi <ekenulokamsi@gmail.com>
2025-11-04 00:59:13 +0100

    Delete week-6/practice_1/practice_2/practice_3/practice_4/practice_5/practice_6/practice_7/practice_8/practice_9/practice_10 directory

--- week-6/practice_1/practice_2/practice_3/practice_4/practice_5/practice_6/practice_7/practice_8/practice_9/practice_10/target/debug/deps/practice_10-310eef80e8dd2b1d.d
+++
@@ -1,5 +0,0 @@
-/home/ekenulokamsi/k.ekenuloCOS101/week-6/practice_1/practice_2/practice_3/practice_4/practice_5/practice_6/practice_7/practice_8/practice_9/practice_10/target/debug/deps/practice_10-310eef80e8dd2b1d.d: src/main.rs
-
-/home/ekenulokamsi/k.ekenuloCOS101/week-6/practice_1/practice_2/practice_3/practice_4/practice_5/practice_6/practice_7/practice_8/practice_9/practice_10/target/debug/deps/practice_10-310eef80e8dd2b1d: src/main.rs
-
-src/main.rs:
