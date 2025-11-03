16145929843e7cbc22fad2a18ebd34db20f88f8f
ekenulokamsi <ekenulokamsi@gmail.com>
2025-11-04 00:56:43 +0100

    week 6

---
+++ week-6/practice_1/practice_2/practice_3/practice_4/practice_5/practice_6/practice_7/practice_8/practice_9/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+/home/ekenulokamsi/k.ekenuloCOS101/week-6/practice_1/practice_2/practice_3/practice_4/practice_5/practice_6/practice_7/practice_8/practice_9/practice_10/target/debug/practice_10: /home/ekenulokamsi/k.ekenuloCOS101/week-6/practice_1/practice_2/practice_3/practice_4/practice_5/practice_6/practice_7/practice_8/practice_9/practice_10/src/main.rs
